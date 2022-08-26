bf6c2c2d2f2ee696cd2c6d55a72dfbb7ac36d5d1
unknown <rafaelherdt1@gmail.com>
2022-08-26 10:10:11 -0300

    toggle button

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,4 +5,15 @@ button.addEventListener('click', switchTheme);
 function switchTheme() {
     document.body.classList.toggle('light-theme');
     document.body.classList.toggle('dark-theme');
+
+    const theme = document.body.classList[0];
+    const themeButton = document.getElementById('themeButton');
+
+    if(theme === 'light-theme') {
+        themeButton.classList.toggle('fa-sun');
+        themeButton.classList.toggle('fa-moon-o');
+    }else{
+        themeButton.classList.toggle('fa-sun');
+        themeButton.classList.toggle('fa-moon-o');
+    }
 }
